c015fc215f15c3b5942effdf77731c17634a40b2
SERGIO TEJEDOR MORENO <119494713+sertemo@users.noreply.github.com>
2024-05-08 09:26:32 +0200

    modificado test que falla en el workflow

--- tests/test_endpoints/test_predictions.py
+++ tests/test_endpoints/test_predictions.py
@@ -4,15 +4,20 @@
 import mlhomeserver.settings as settings
 
 def test_predict_with_csv_aidtec(client):
-    # Suponiendo que 'example.csv' es un archivo válido para pruebas
-    with open(settings.DATA_PATH / "aidtec" / "test.csv", 'rb') as file:
-        response = client.post(
-            "/predict/aidtec",
-            files={"file": ("filename.csv", file, "text/csv")}
-        )
-    assert response.status_code == 201
-    assert response.json()["status"] == "OK"
-    assert "predictions" in response.json()["data"]
+    try:
+        # Suponiendo que 'example.csv' es un archivo válido para pruebas
+        with open(settings.DATA_PATH / "aidtec" / "test.csv", 'rb') as file:
+            response = client.post(
+                "/predict/aidtec",
+                files={"file": ("filename.csv", file, "text/csv")}
+            )
+        assert response.status_code == 201
+        assert response.json()["status"] == "OK"
+        assert "predictions" in response.json()["data"]
+    except Exception as e:
+        print(f"Error during test execution: {e}")
+        print(f"Current DATA_PATH: {settings.DATA_PATH}")
+        raise
 
 
 def test_predict_with_invalid_file_format(client):

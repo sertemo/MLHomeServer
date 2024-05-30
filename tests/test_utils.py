d1d1398dd668e66fb1971e22f189230e5b3ec46e
sertemo <tejedor.moreno@gmail.com>
2024-05-30 22:02:51 +0200

    mejorado el test de utils para la versión mockeando

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,32 @@
 
+from unittest.mock import patch, mock_open
+
+import pytest
+
 from mlhomeserver.utils import get_project_version, get_current_competitions_from_yml
 
-def test_get_project_version():
-    assert get_project_version() == '0.1.3'
+def test_get_project_version_success():
+    mock_toml_content = """
+    [tool.poetry]
+    version = "1.2.0"
+    """
+    with patch("builtins.open", mock_open(read_data=mock_toml_content)):
+        with patch("toml.load", return_value={"tool": {"poetry": {"version": "1.2.0"}}}):
+            assert get_project_version() == "1.2.0"
+
+def test_get_project_version_missing_key():
+    mock_toml_content = """
+    [tool.poetry]
+    """
+    with patch("builtins.open", mock_open(read_data=mock_toml_content)):
+        with patch("toml.load", return_value={"tool": {"poetry": {}}}):
+            with pytest.raises(KeyError):
+                get_project_version()
+
+def test_get_project_version_file_not_found():
+    with patch("builtins.open", side_effect=FileNotFoundError):
+        with pytest.raises(FileNotFoundError):
+            get_project_version()
 
 def test_get_current_competitions_correct():
     assert get_current_competitions_from_yml() == ['aidtec']
